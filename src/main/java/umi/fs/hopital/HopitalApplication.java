832ef34a08c0be282885e26bfd317d5142214d44
Dassin2001 <fatimaamir012345@gmail.com>
2025-05-30 16:35:20 +0100

    Premier Test

--- src/main/java/umi/fs/hopital/HopitalApplication.java
+++ src/main/java/umi/fs/hopital/HopitalApplication.java
@@ -1,15 +1,28 @@
 package umi.fs.hopital;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import umi.fs.hopital.entities.Patient;
+import umi.fs.hopital.repository.PateintRepository;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class HopitalApplication implements CommandLineRunner {
-
+    @Autowired
+    private PateintRepository pateintRepository;
     public static void main(String[] args) {
         SpringApplication.run(HopitalApplication.class, args);
     }
     //teste
+    @Override
+    public void run(String... args) throws Exception {
+        pateintRepository.save(new Patient(null,"Yassine",new Date(),false,34));
+        pateintRepository.save(new Patient(null,"Hanane",new Date(),false,4321));
+        pateintRepository.save(new Patient(null,"Yassine",new Date(),true,34));
+
+    }
 
 }
--- src/main/java/umi/fs/hopital/entities/Patient.java
+++ src/main/java/umi/fs/hopital/entities/Patient.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,6 +16,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
